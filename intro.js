d6e6538f6f092a07a4ea4f4bd9891e5c6b4e0e27
Benjamin Acheampong <acheampongb530@gmail.com>
2026-03-13 23:15:39 +0000

    Initial commit: First assignment

---
+++ intro.js
@@ -0,0 +1,9 @@
+// Variables
+const name = "Benjamin";
+const age = 29;
+const favoritePL = "Javascript";
+
+// Log to console
+console.log("Name: ", name);
+console.log("Age: ", age);
+console.log("Favorite Programming Language: ", favoritePL)
